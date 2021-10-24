772a0fd6524d3c1487dce6a60fbb57dc228e67bc
Mohammad Wahid <wahid.warid55@gmail.com>
2021-10-24 12:41:25 +0600

    Laravel Breez; Bootstrap 5 added

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,13 @@
-const mix = require('laravel-mix');
+const crypto = require("crypto");
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+let theme = process.env.npm_config_theme;
+
+if(theme) {
+    require(`${__dirname}/themes/${theme}/webpack.mix.js`);
+} else {
+    require(`${__dirname}/themes/default/webpack.mix.js`);
+}
+
