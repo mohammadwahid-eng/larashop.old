d7bcedf5ee32621f7db89bfbb41c0c07c0ade7fb
Mohammad Wahid <m.wahid@rootnext.com>
2021-10-29 19:56:00 +0600

    Admin: In Progress

--- routes/auth.php
+++ routes/auth.php
@@ -10,65 +10,74 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('customer')->middleware(['theme:default'])->group(function() {
+Route::prefix('customer')->name('customer.')->middleware(['theme:default'])->group(function() {
 
     Route::middleware(['guest'])->group(function () {
-        Route::get('/registration', [RegisteredUserController::class, 'create'])->name('customer.registration');
+        Route::get('/registration', [RegisteredUserController::class, 'create'])->name('registration');
         Route::post('/registration', [RegisteredUserController::class, 'store']);
 
-        Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('customer.login');
+        Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
         Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 
-        Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])->name('customer.password.request');
-        Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->name('customer.password.email');
+        Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
+        Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
 
-        Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])->name('customer.password.reset');
-        Route::post('/reset-password', [NewPasswordController::class, 'store'])->name('customer.password.update');
+        Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
+        Route::post('/reset-password', [NewPasswordController::class, 'store'])->name('password.update');
     });
 
     Route::middleware(['auth'])->group(function () {
-        Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])->name('customer.verification.notice');
-        Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])->middleware(['signed', 'throttle:6,1'])->name('customer.verification.verify');
-        Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])->middleware(['throttle:6,1'])->name('customer.verification.send');
+        Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])->name('verification.notice');
+        Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])->middleware(['signed', 'throttle:6,1'])->name('verification.verify');
+        Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])->middleware(['throttle:6,1'])->name('verification.send');
 
-        Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])->name('customer.password.confirm');
+        Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])->name('password.confirm');
         Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store']);
 
-        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('customer.logout');
+        Route::middleware(['verified'])->group(function() {
+            Route::view('/', 'customer.index')->name('home');
+            Route::view('/orders', 'customer.orders.index')->name('orders');
+            Route::view('/downloads', 'customer.downloads.index')->name('downloads');
+            Route::view('/addresses', 'customer.addresses.index')->name('addresses');
+            Route::view('/payment-methods', 'customer.payment-methods.index')->name('payment_methods');
+            Route::view('/account', 'customer.account.index')->name('account');
+        });
+
+        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
     });
 
 });
 
 
-Route::prefix('admin')->middleware(['theme:admin'])->group(function() {
+Route::prefix('admin')->name('admin.')->middleware(['theme:admin'])->group(function() {
 
     Route::middleware(['guest:admin'])->group(function () {
-        Route::get('/registration', [RegisteredUserController::class, 'create'])->name('admin.registration');
+        Route::get('/registration', [RegisteredUserController::class, 'create'])->name('registration');
         Route::post('/registration', [RegisteredUserController::class, 'store']);
 
-        Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('admin.login');
+        Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
         Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 
-        Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])->name('admin.password.request');
-        Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->name('admin.password.email');
+        Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
+        Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
 
-        Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])->name('admin.password.reset');
-        Route::post('/reset-password', [NewPasswordController::class, 'store'])->name('admin.password.update');
+        Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
+        Route::post('/reset-password', [NewPasswordController::class, 'store'])->name('password.update');
     });
 
     Route::middleware(['auth:admin'])->group(function () {
-        Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])->name('admin.verification.notice');
-        Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])->middleware(['signed', 'throttle:6,1'])->name('admin.verification.verify');
-        Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])->middleware(['throttle:6,1'])->name('admin.verification.send');
+        Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])->name('verification.notice');
+        Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])->middleware(['signed', 'throttle:6,1'])->name('verification.verify');
+        Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])->middleware(['throttle:6,1'])->name('verification.send');
 
-        Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])->name('admin.password.confirm');
+        Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])->name('password.confirm');
         Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store']);
 
-        Route::middleware(['verified'])->group(function () {
-            Route::view('/', 'home')->name('admin.home');
+        Route::middleware(['verified'])->group(function() {
+            Route::view('/', 'index')->name('home');
         });
 
-        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('admin.logout');
+        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
     });
 
 });
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'index');
+Route::view('/shop', 'products.list.index')->name('shop');
+Route::view('/cart', 'cart.index')->name('cart');
+Route::view('/checkout', 'checkout.index')->name('checkout');
+Route::view('/compare', 'compare.index')->name('compare');
+Route::view('/wishlist', 'wishlist.index')->name('wishlist');
 
 require __DIR__.'/auth.php';
-require __DIR__.'/shop.php';
